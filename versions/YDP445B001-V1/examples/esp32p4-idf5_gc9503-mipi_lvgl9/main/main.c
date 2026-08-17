53a9a1c2c883745cd2684d06e2782a6590d4cef6
BLSNCD <2857585364@qq.com>
2026-08-17 09:38:03 +0800

    docs(examples): add OSPTEK SPDX header to example entry main.c

--- versions/YDP445B001-V1/examples/esp32p4-idf5_gc9503-mipi_lvgl9/main/main.c
+++ versions/YDP445B001-V1/examples/esp32p4-idf5_gc9503-mipi_lvgl9/main/main.c
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 OSPTEK
+ * SPDX-License-Identifier: CC-BY-4.0
+ *
+ * https://github.com/osptek
+ */
+
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
